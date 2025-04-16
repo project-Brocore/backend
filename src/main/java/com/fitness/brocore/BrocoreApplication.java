2b7732be469a81a505d484fdcc611f7669585e30
zoomaxeu <dandimir1@seznam.cz>
2025-04-16 22:55:24 +0200

    basic structure

---
+++ src/main/java/com/fitness/brocore/BrocoreApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.brocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrocoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrocoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitness/brocore/BrocoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.brocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrocoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
